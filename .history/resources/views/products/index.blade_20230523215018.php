ec4d3977e8434dbccfc6771dfb5dade0c84d2f73
satoshi514 <satoshiy4@gmail.com>
2023-05-28 18:08:17 +0900

    できるかな

---
+++ .history/resources/views/products/index.blade_20230523215018.php
@@ -0,0 +1,26 @@
+<a href="{{ route('products.create') }}"> Create New Product</a>
+
+<table>
+  <tr>
+    <th>Name</th>
+    <th>Description</th>
+    <th>Price</th>
+    <th>Category_ID</th>
+    <th>Action</th>
+  </tr>
+  @foreach($products as $product)
+  <tr>
+    <td>{{ $product->name }}</td>
+    <td>{{ $product->description }}</td>
+    <td>{{ $product->price }}</td>
+    <td>{{ $product->category_id }}</td>
+    <td>
+         <form action= "{{ route('products.destroy',$product->id) }}" method="POST">
+          @csrf
+          @method('DELETE')
+          <button type="submit">Delete</button>
+         </form>
+    </td>
+  </tr>
+  @endforeach
+</table>
